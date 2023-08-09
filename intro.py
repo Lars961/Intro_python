0b37733e11bae5b41f17533fb103d23d2c6a5371
Lars961 <alam11_@hotmail.com>
2023-08-08 18:18:41 -0600

    first commit

---
+++ intro.py
@@ -0,0 +1,31 @@
+#Asi se realizan comentarios en python
+if 3>5:
+    print('Esto no se va a imprimir')
+
+if 5>3:
+    print('5 es mayor a 3')
+
+x = 5
+y = 'prueba de variables'
+
+print(x,y)
+
+email = 'lars@mail.com'
+
+print (email)
+
+#formas de crear variables
+_mi_var = 'Variable 1\n'
+MIVAR = 'Variable 2\n'
+a, b, c = 'variable a\n','variable b\n','variable c\n'
+
+print(_mi_var, MIVAR, a, b, c)
+
+valor1 = valor2 = valor3 = 'hola mundo en python xD'
+
+print(valor1 + '\n' + valor2 + '\n' + valor3 + '\n')
+
+inicio = 'Hola'
+final = 'mundo'
+
+print(inicio + ' ' + final)
