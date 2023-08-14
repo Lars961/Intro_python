8ad7dfe4d664e1902c3c89ba7ecb652bd9ef49fa
Lars961 <alam11_@hotmail.com>
2023-08-14 17:20:18 -0600

    done

---
+++ control_flujo.py
@@ -0,0 +1,55 @@
+#Ciclo If 
+#if 2<5 :
+    #print('2 es menor que 5')
+
+#Doble igualdad == 
+#menor que <
+#mayor que >
+#diferente de !=
+#menor igual a <=
+#mayor igual >=
+
+# if 2 == 2:
+#     print('2 es igual a 2')
+
+# if 2 == 3:
+#     print("2 es igual a 3 ")
+
+# if 2 > 5:
+#     print('2 es mayor a 5')
+
+# if 5 > 2:
+#     print('5 es mayor a 2')
+
+# if 2 != 2:
+#     ('2 es distinto de 2')
+
+# if 3 != 2:
+#     print('3 es distinto de 2')
+
+# if 3>= 2:
+#     print('3 es mayor a 2')
+
+# if 3 <= 3 :
+#     print('3 es menor o igual a 3')
+
+#ciclo elif
+if 2>5 :
+    print('2 es mayor a 5')
+elif 2>5 :
+    print('2 es menor a 5')
+else:
+    print('si todo lo anterior es falso, me imprimo')
+
+#if else
+if 2>5 :
+    print('2 es mayor a 5')
+
+else:
+    print('si todo lo anterior es falso, me imprimo 2')
+
+#if de una sola linea con operador ternario
+if 2 < 5: print('if de una sola linea')
+print ('cuando devuelve true') if 5 > 2 else print('cuando devuelve false')
+
+# and y or 
